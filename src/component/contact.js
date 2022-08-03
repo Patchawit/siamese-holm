06095852dfa473c78fbe473e1720773ac77bc6c1
patchawitdigi <110367084+patchawitdigi@users.noreply.github.com>
2022-08-03 13:10:34 +0700

    responsive

--- src/component/contact.js
+++ src/component/contact.js
@@ -46,7 +46,7 @@ export default function contact() {
                     </div>
                     <div className='col-sm-6 col-12'>
                         <div className='map'>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3871.0194849147806!2d100.61919795021066!3d14.016858990128197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xec32f7aa96e3a1c4!2zMTTCsDAxJzAwLjciTiAxMDDCsDM3JzE3LjAiRQ!5e0!3m2!1sen!2sth!4v1659412602761!5m2!1sen!2sth" id="ggmap" frameborder="0" style={{ border: 0 }} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3871.0194849147806!2d100.61919795021066!3d14.016858990128197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xec32f7aa96e3a1c4!2zMTTCsDAxJzAwLjciTiAxMDDCsDM3JzE3LjAiRQ!5e0!3m2!1sen!2sth!4v1659412602761!5m2!1sen!2sth" id="ggmap" frameBorder="0" style={{ border: 0 }} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
                         </div>
                     </div>
                 </div>
--- src/component/formregis.js
+++ src/component/formregis.js
@@ -1,32 +1,64 @@
 import './formregis.css';
 import bg from "../img/BG.png";
-import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 
 
 
-export default function formregis() {
+export default function Formregis() {
+
+    const [name,setName] = useState('')
+    const [surname,setSurname] = useState('')
+    const [number,setNumber] = useState('')
+    const [email,setEmail] = useState('')
+    const [design,setDesign] = useState('')
+    const [budget,setBudget] = useState('')
+
+    const inputName = (event) =>{
+        setName(event.target.value)
+    }
+    const inputSurname = (event) =>{
+        setSurname(event.target.value)
+    }
+    const inputNumber = (event) =>{
+        setNumber(event.target.value)
+    }
+    const inputEmail = (event) =>{
+        setEmail(event.target.value)
+    }
+    const inputDesign = (event) =>{
+        setDesign(event.target.value)
+    }
+    const inputBudget = (event) =>{
+        setBudget(event.target.value)
+    }
+    const saveInfo = (event) =>{
+        event.preventDefault()
+
+    }
+
+
     return (
         <div className='regis'>
             <img src={bg} />
             <p>เตรียมพบบ้านเดี่ยว-ทาวน์โฮม โครงการใหม่<br />ติดถนนใหญ่ใกล้รถไฟฟ้า และฟิวเจอร์พาร์ครังสิต เพียง 4 นาที</p>
             <h1>ลงทะเบียนเพื่อรับสิทธิพิเศษ</h1>
-            <form>
+            <form onSubmit={saveInfo}>
                 <div className='container-fluid' style={{width:"75vw"}}>
                     <div className="row textinput">
                         <div className="col-sm-6 col-12">
-                            <input type="text" placeholder='ชื่อ*'></input>
+                            <input type="text" placeholder='ชื่อ*' onChange={inputName}></input>
                         </div>
                         <div className="col-sm-6 col-12">
-                            <input type="text" placeholder='นามสกุล*'></input>
+                            <input type="text" placeholder='นามสกุล*' onChange={inputSurname}></input>
                         </div>
                     </div>
                     <div className="row textinput">
                         <div className="col-sm-6 col-12">
-                            <input type="text" placeholder='เบอร์โทรศัพท์*'></input>
+                            <input type="text" placeholder='เบอร์โทรศัพท์*' onChange={inputNumber}></input>
                         </div>
                         <div className="col-sm-6 col-12">
-                            <input type="text" placeholder='อีเมล'></input>
+                            <input type="text" placeholder='อีเมล' onChange={inputEmail}></input>
                         </div>
                     </div>
                 </div>
@@ -34,19 +66,20 @@ export default function formregis() {
                 <div className='container-fluid' style={{width:"75vw"}}>
                     <div className="row opinput">
                         <div className="col-sm-6 col-12">
-                            <select placeholder='แบบบ้านที่สนใจ*'>
+                            <select id="mydesign" placeholder='แบบบ้านที่สนใจ*' onChange={inputDesign}>
                                 <option value="0">แบบบ้านที่สนใจ*</option>
                             </select>
                         </div>
                         <div className="col-sm-6 col-12">
-                            <select placeholder='งบประมาณ*'>
+                            <select id="mybudget" placeholder='งบประมาณ*' onChange={inputBudget}>
                                 <option value="0">งบประมาณ*</option>
                             </select>
                         </div>
                     </div>
                 </div>
+                <button type="submit">ลงทะเบียน</button>
             </form>
-            <button type="button">ลงทะเบียน</button>
+            
         </div>
     )
 }
