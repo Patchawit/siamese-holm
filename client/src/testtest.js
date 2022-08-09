e1cad90c787314a12256a3d1d53a77b7ed5b875b
patchawitdigi <110367084+patchawitdigi@users.noreply.github.com>
2022-08-09 15:42:20 +0700

    test

--- client/src/testtest.js
+++ client/src/testtest.js
@@ -1,33 +1,238 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
 
 
-export default function Testtest() {
+// export default function Testtest() {
 
-    const [users, setUsers] = useState({})
-    const [hasError, setErrors] = useState(false)
+//     const [users, setUsers] = useState({})
+//     const [hasError, setErrors] = useState(false)
 
-    async function loadData() {
-        const res = await fetch('https://randomuser.me/api/');
-        res
-            .json()
-            .then(res => setUsers(res))
-            .catch(err => setErrors(err));
-    }
-    console.log(JSON.stringify(users))
+//     async function loadData() {
+//         const res = await fetch('https://randomuser.me/api/');
+//         res
+//             .json()
+//             .then(res => setUsers(res))
+//             .catch(err => setErrors(err));
+//     }
+//     console.log(JSON.stringify(users))
+
+//     useEffect(() => {
+//         loadData();
+//     }, [])
+
+
+
+//     return (
+//         <div>
+//             <span>{JSON.stringify(users)}</span>
+//             <hr></hr>
+//             <span>{JSON.stringify(hasError)}</span>
+//         </div>
+
+//     )
+// }
+
+
+import { useEffect, useState } from 'react';
+import Axios from 'axios'
+import $ from "jquery";
+import { Link } from 'react-router-dom';
+
+
+
+export default function Formregis() {
+
+    // const [registerList, setregisterList] = useState([])
+    // const [result, setResult] = useState("");
+
+
+    const [firstName, setfisrtName] = useState("");
+    const [surName, setsurName] = useState("");
+    const [numberp, setNumberp] = useState("");
+    const [email, setEmail] = useState("");
+    const [design, setDesign] = useState("");
+    const [budget, setBudget] = useState("");
+
+    // const handleSumbit = (e) => {
+    //     e.preventDefault();
+    //     const form = $(e.target);
+    //     $.ajax({
+    //         type: "POST",
+    //         url: form.attr("action"),
+    //         data: form.serialize(),
+    //         success(data) {
+    //             setResult(data);
+    //         },
+    //     });
+    //     // window.location.href="/#/thankyou"; 
+    //     console.log("success")
+    // };
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await fetch("https://digitivework.com/project/siamese/PHP/index.php", {
+                method: "POST",
+                body: JSON.stringify({
+                    firstName: firstName,
+                    surName: surName,
+                    numberp: numberp,
+                    email: email,
+                    design: design,
+                    budget: budget
+                }),
+            });
+        } catch (err) {
+            console.log(err);
+        }
+        console.log(firstName, surName)
+    };
+
+    // const addRegis = () => {
+    //     Axios.post("http://localhost:3001/addregister", {
+    //         firstName: firstName,
+    //         surName: surName,
+    //         number: number,
+    //         email: email,
+    //         design: design,
+    //         budget: budget
+    //     }).then(() => {
+    //         setregisterList([
+    //             ...registerList,
+    //             {
+    //                 firstName: firstName,
+    //                 surName: surName,
+    //                 number: number,
+    //                 email: email,
+    //                 design: design,
+    //                 budget: budget
+    //             },
+    //         ]);
+    //     });
+    // };
+
+
+
 
     useEffect(() => {
-        loadData();
+
+        var subjectObject = {
+            "แบบบ้านที่สนใจ*": [],
+            "บ้านเดี่ยว": ["6-7 ล้านบาท", "7-8 ล้านบาท", "8-9 ล้านบาท", "9-10 ล้านบาท", "10-11 ล้านบาท", "11-12 ล้านบาท", "มากกว่า 12 ล้านบาท"],
+            "บ้านแฝด": ["2-3 ล้านบาท", "3-4 ล้านบาท", "4-5 ล้านบาท", "5-6 ล้านบาท", "มากกว่า 6 ล้านบาท"],
+            "ทาวน์โฮม": ["2-3 ล้านบาท", "3-4 ล้านบาท", "4-5 ล้านบาท", "5-6 ล้านบาท", "มากกว่า 6 ล้านบาท"]
+        }
+        document.querySelector('#mydesign').innerHTML = '';
+
+        var subjectSel = document.getElementById("mydesign");
+        var topicSel = document.getElementById("mybudget");
+        for (var x in subjectObject) {
+            subjectSel.options[subjectSel.options.length] = new Option(x, x);
+
+        }
+
+        subjectSel.onchange = function () {
+            topicSel.length = 1;
+
+            for (var y in subjectObject[this.value]) {
+                topicSel.options[topicSel.options.length] = new Option(subjectObject[this.value][y], subjectObject[this.value][y]);
+            }
+        }
+
+
     }, [])
 
-   
 
     return (
-        <div>
-            <span>{JSON.stringify(users)}</span>
-            <hr></hr>
-            <span>{JSON.stringify(hasError)}</span>
-        </div>
+        <div className='regis'>
 
+            <p>เตรียมพบบ้านเดี่ยว-ทาวน์โฮม โครงการใหม่<br />ติดถนนใหญ่ใกล้รถไฟฟ้า และฟิวเจอร์พาร์ครังสิต เพียง 4 นาที</p>
+            <h1>ลงทะเบียนเพื่อรับสิทธิพิเศษ</h1>
+            <form  //http://localhost:8000/server.php
+                
+                onSubmit={(event) => handleSubmit(event)}>
+                <div className='container-fluid' style={{ width: "75vw" }}>
+                    <div className="row textinput">
+                        <div className="col-sm-6 col-12">
+                            <div class="form-group">
+                                <input className='form-input' placeholder=' ' autoComplete='off' required
+                                    type="text"
+                                    name="firstName"
+                                    onChange={(event) => {
+                                        setfisrtName(event.target.value)
+                                    }}>
+                                </input>
+                                <label className='form-label'>ชื่อ*</label>
+                            </div>
+                        </div>
+                        <div className="col-sm-6 col-12">
+                            <div class="form-group">
+
+                                <input className='form-input' placeholder=' ' autoComplete='off' required
+                                    type="text"
+                                    name="surName"
+                                    onChange={(event) => {
+                                        setsurName(event.target.value)
+                                    }}>
+                                </input>
+                                <label className='form-label'>นามสกุล*</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row textinput">
+                        <div className="col-sm-6 col-12">
+                            <div class="form-group">
+                                <input className='form-input' placeholder=' ' autoComplete='off' required
+                                    type="text"
+                                    name="numberp"
+                                    onChange={(event) => {
+                                        setNumberp(event.target.value)
+                                    }}>
+                                </input>
+                                <label className='form-label'>เบอร์โทรศัพท์*</label>
+                            </div>
+                        </div>
+                        <div className="col-sm-6 col-12">
+                            <div class="form-group">
+                                <input className='form-input' placeholder=' ' autoComplete='off'
+                                    type="text"
+                                    name="email"
+                                    onChange={(event) => {
+                                        setEmail(event.target.value)
+                                    }}>
+                                </input>
+                                <label className='form-label'>อีเมล</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='container-fluid' style={{ width: "75vw" }}>
+                    <div className="row opinput">
+                        <div className="col-sm-6 col-12">
+                            <select id="mydesign" required
+                                name="design"
+                                onChange={(event) => {
+                                    setDesign(event.target.value)
+                                }}>
+                                <option value="">แบบบ้านที่สนใจ*</option>
+                            </select>
+
+                        </div>
+                        <div className="col-sm-6 col-12">
+                            <select id="mybudget" required
+                                name="budget"
+                                onChange={(event) => {
+                                    setBudget(event.target.value)
+                                }}>
+                                <option value="">งบประมาณ*</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                {/* <Link to="/thankyou"> */}
+                <button type="submit">ลงทะเบียน</button>
+                {/* </Link> */}
+            </form>
+        </div>
     )
 }
-
